b6370da0833a325bd50ca3b9fdd2a70c64487f64
ZLHoussem <houssemeddinzlitni18@gmail.com>
2024-06-11 23:30:05 +0100

    jj uu

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -11,14 +11,14 @@ import { RevenueChartSkeleton,LatestInvoicesSkeleton,CardsSkeleton } from '@/app
 export default async function Page() {
   {/*const revenue = await fetchRevenue();
     const latestInvoices = await fetchLatestInvoices();*/}
-  
+  {/* 
   const {
     numberOfInvoices,
     numberOfCustomers,
     totalPaidInvoices,
     totalPendingInvoices,
   } = await fetchCardData();
- 
+  */}
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
